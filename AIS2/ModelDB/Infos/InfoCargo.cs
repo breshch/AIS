e3a04c2b8946171517ba88ceefb61d536c0d26f8
Ivan <toporischev@gmail.com>
2014-02-06 15:30:59 +0600

    Добавил папку Infos

---
+++ AIS2/ModelDB/Infos/InfoCargo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelDB.Infos
+{
+    public class InfoCargo
+    {
+        public int Id { get; set; }
+        public string Basement { get; set; }
+
+        public int DirectoryRCId { get; set; }
+        public DirectoryRC DirectoryRC { get; set; }
+
+        public int DirectoryPoDId { get; set; }
+        public DirectoryPoD DirectoryPoD { get; set; }
+
+        public double Weight { get; set; }
+        public double? Length { get; set; }
+        public double Volume { get; set; }
+        public double Sum { get; set; }
+    }
+}
---
+++ AIS2/ModelDB/Infos/InfoCompany.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelDB.Infos
+{
+    public class InfoCompany
+    {
+        public int Id { get; set; }
+
+        public int DirectoryCompanyId { get; set; }
+        public DirectoryCompany DirectoryCompany { get; set; }
+
+        public ICollection<InfoWorker> InfoWorkers { get; set; }
+        public ICollection<InfoPermitForCar> InfoPermitForCars { get; set; }
+    }
+}
