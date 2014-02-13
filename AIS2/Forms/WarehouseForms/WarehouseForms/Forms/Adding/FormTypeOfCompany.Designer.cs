8b0344b727ad21e6021bd5312d9afb40b29a5d19
Alexey Breshchenko <breshch@gmail.com>
2014-02-13 11:06:18 +0400

    Создали форму тип компаниии

---
+++ AIS2/Forms/WarehouseForms/WarehouseForms/Forms/Adding/FormTypeOfCompany.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WarehouseForms.Forms.Adding
+{
+    partial class FormTypeOfCompany
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.textBoxTypeOfCompany = new System.Windows.Forms.TextBox();
+            this.listBoxTypeOfCompanies = new System.Windows.Forms.ListBox();
+            this.labelTypeOfCompany = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(151, 173);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(121, 23);
+            this.buttonRemove.TabIndex = 7;
+            this.buttonRemove.Text = "Удалить";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(12, 173);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(121, 23);
+            this.buttonAdd.TabIndex = 6;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // textBoxTypeOfCompany
+            // 
+            this.textBoxTypeOfCompany.Location = new System.Drawing.Point(12, 140);
+            this.textBoxTypeOfCompany.Name = "textBoxTypeOfCompany";
+            this.textBoxTypeOfCompany.Size = new System.Drawing.Size(260, 20);
+            this.textBoxTypeOfCompany.TabIndex = 5;
+            // 
+            // listBoxTypeOfCompanies
+            // 
+            this.listBoxTypeOfCompanies.FormattingEnabled = true;
+            this.listBoxTypeOfCompanies.Location = new System.Drawing.Point(12, 12);
+            this.listBoxTypeOfCompanies.Name = "listBoxTypeOfCompanies";
+            this.listBoxTypeOfCompanies.Size = new System.Drawing.Size(260, 95);
+            this.listBoxTypeOfCompanies.TabIndex = 4;
+            // 
+            // labelTypeOfCompany
+            // 
+            this.labelTypeOfCompany.AutoSize = true;
+            this.labelTypeOfCompany.Location = new System.Drawing.Point(12, 124);
+            this.labelTypeOfCompany.Name = "labelTypeOfCompany";
+            this.labelTypeOfCompany.Size = new System.Drawing.Size(79, 13);
+            this.labelTypeOfCompany.TabIndex = 8;
+            this.labelTypeOfCompany.Text = "Тип компании";
+            // 
+            // FormTypeOfCompany
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 224);
+            this.Controls.Add(this.labelTypeOfCompany);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxTypeOfCompany);
+            this.Controls.Add(this.listBoxTypeOfCompanies);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "FormTypeOfCompany";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Тип компаний";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormTypeOfCompany_FormClosing);
+            this.Load += new System.EventHandler(this.FormTypeOfCompany_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.TextBox textBoxTypeOfCompany;
+        private System.Windows.Forms.ListBox listBoxTypeOfCompanies;
+        private System.Windows.Forms.Label labelTypeOfCompany;
+    }
+}
---
+++ AIS2/Forms/WarehouseForms/WarehouseForms/Forms/Adding/FormTypeOfCompany.cs
@@ -0,0 +1,119 @@
+﻿using ModelDB;
+using ModelDB.Directories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WarehouseForms.Forms.Adding
+{
+    public partial class FormTypeOfCompany : Form
+    {
+        private Context _db = new Context();
+
+        public FormTypeOfCompany()
+        {
+            InitializeComponent();
+        }
+
+        private void FormTypeOfCompany_Load(object sender, EventArgs e)
+        {
+            listBoxTypeOfCompanies.Items.Clear();
+            FormFill();
+        }
+
+        private void FormFill()
+        {
+            ClearForm();
+            listBoxTypeOfCompanies.Items.AddRange(_db.DirectoryTypeOfCompanies.Select(t => t.Name).ToArray());
+
+            buttonRemove.Enabled = listBoxTypeOfCompanies.Items.Count > 0 ? true : false;
+        }
+
+        private void FormTypeOfCompany_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _db.Dispose();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (IsValidateAdd())
+            {
+                _db.DirectoryTypeOfCompanies.Add(new DirectoryTypeOfCompany { Name = textBoxTypeOfCompany.Text });
+                _db.SaveChanges();
+
+                ClearForm();
+                FormFill();
+            }
+        }
+
+        private bool IsValidateAdd()
+        {
+            if (!string.IsNullOrWhiteSpace(textBoxTypeOfCompany.Text))
+            {
+                if (!_db.DirectoryTypeOfCompanies.Select(t => t.Name).Contains(textBoxTypeOfCompany.Text))
+                {
+                    return true;
+                }
+                else
+                {
+                    MessageBox.Show("Такой тип компании уже существует." + Environment.NewLine + "Введите другой тип компании.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Заполните тип компании.");
+            }
+
+            return false;
+        }
+
+        private bool IsValidateRemove(DirectoryTypeOfCompany directoryTypeOfCompany)
+        {
+            if (!(_db.DirectoryCompanies.Select(c => c.DirectoryTypeOfCompanyId).Contains(directoryTypeOfCompany.Id)
+                || _db.InfoCompanies.Select(c => c.DirectoryCompany.DirectoryTypeOfCompanyId).Contains(directoryTypeOfCompany.Id)))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("Невозможно удалить данный тип компании," + Environment.NewLine + "так как существует компания, принадлежащая типу "
+                    + directoryTypeOfCompany.Name.ToString());
+            }
+            return false;
+        }
+
+        private void ClearForm()
+        {
+            listBoxTypeOfCompanies.Items.Clear();
+            textBoxTypeOfCompany.Clear();
+        }
+
+        private void textBoxTypeOfCompany_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            if (listBoxTypeOfCompanies.SelectedIndex != -1)
+            {
+                string typeOfCompanies = listBoxTypeOfCompanies.SelectedItem.ToString();
+                DirectoryTypeOfCompany directoryTypeOfCompany = _db.DirectoryTypeOfCompanies.First(t => t.Name == typeOfCompanies);
+
+                if (IsValidateRemove(directoryTypeOfCompany))
+                {
+                    _db.DirectoryTypeOfCompanies.Remove(directoryTypeOfCompany);
+                    _db.SaveChanges();
+
+                    FormFill();
+                }
+            }
+        }
+    }
+}
