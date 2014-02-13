b1fd082af395655558de5e0faf5cec2204072311
Alexey Breshchenko <breshch@gmail.com>
2014-02-13 11:04:39 +0400

    убрали внешний ключ на тип компании

--- AIS2/Models/ModelDB/Infos/InfoCompany.cs
+++ AIS2/Models/ModelDB/Infos/InfoCompany.cs
@@ -14,9 +14,6 @@ namespace ModelDB.Infos
         public int DirectoryCompanyId { get; set; }
         public DirectoryCompany DirectoryCompany { get; set; }
 
-        public int DirectoryTypeOfCompanyId { get; set; }
-        public DirectoryTypeOfCompany DirectoryTypeOfCompany { get; set; }
-
         public ICollection<InfoWorker> InfoWorkers { get; set; }
         public ICollection<InfoPermitForCar> InfoPermitForCars { get; set; }
     }
