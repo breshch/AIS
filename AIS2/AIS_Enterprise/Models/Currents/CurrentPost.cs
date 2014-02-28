11ceac94f49cc7bdd53c67841c983b9b56556785
Alexey Breshchenko <breshch@gmail.com>
2014-02-28 09:16:00 +0400

    добавили CurrentPost

---
+++ AIS2/AIS_Enterprise/Models/Currents/CurrentPost.cs
@@ -0,0 +1,19 @@
+﻿using AIS_Enterprise.Models.Directories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIS_Enterprise.Models.Currents
+{
+    public class CurrentPost
+    {
+        public int Id { get; set; }
+        public DateTime ChangeDate { get; set; }
+        public DateTime? FireDate { get; set; }
+
+        public int DirectoryPostId { get; set; }
+        public virtual DirectoryPost DirectoryPost { get; set; }
+    }
+}
