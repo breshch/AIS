d90f58b7d3bf34a5d6ab04d01d84c36ae1c59ba9
Alexey Breshchenko <breshch@gmail.com>
2014-02-28 12:55:48 +0400

    Добавили конвертер Gender to bool and bool to gender

---
+++ AIS2/AIS_Enterprise/Helpers/ValueConverters/GenderToBoolValueConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace AIS_Enterprise.Helpers.ValueConverters
+{
+    public class GenderToBoolValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Gender gender = (Gender) Enum.Parse(typeof(Gender), value.ToString());
+
+            return gender == Gender.Male;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return bool.Parse(value.ToString()) ? Gender.Male : Gender.Female;
+        }
+    }
+}
