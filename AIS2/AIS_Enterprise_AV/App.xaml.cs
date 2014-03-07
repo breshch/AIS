2f1a942c6e39e7576c639ccc826db7b588c3f02c
Alexey Breshchenko <breshch@gmail.com>
2014-03-07 13:26:36 +0400

    Добавили проект AV

---
+++ AIS2/AIS_Enterprise_AV/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using AIS_Enterprise_Global.Models;
+using AIS_Enterprise_Global.ViewModels;
+using AIS_Enterprise_Global.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AIS_Enterprise_AV
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<AIS_Enterprise_Global.Models.DataContext>());
+
+            using (var bc = new BusinessContext())
+            {
+                bc.InitializeDatabase();
+            }
+
+            var mainViewModel = new MainViewModel();
+            var mainView = new MainView();
+
+            mainView.DataContext = mainViewModel;
+            mainView.ShowDialog();
+        }
+    }
+}
---
+++ AIS2/AIS_Enterprise_AV/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AIS_Enterprise_AV
+{
+    class DataContext
+    {
+    }
+}
