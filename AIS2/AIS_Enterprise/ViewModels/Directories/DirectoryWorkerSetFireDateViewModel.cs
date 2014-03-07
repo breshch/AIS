2fed0ecc13c5e4edd4f3bdf5aa85c28cf12b9013
Alexey Breshchenko <breshch@gmail.com>
2014-03-07 12:16:33 +0400

    Добавили FormSetFireDate

---
+++ AIS2/AIS_Enterprise/ViewModels/Directories/DirectoryWorkerSetFireDateViewModel.cs
@@ -0,0 +1,67 @@
+﻿using AIS_Enterprise.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AIS_Enterprise.ViewModels.Directories
+{
+    public class DirectoryWorkerSetFireDateViewModel : ViewModel
+    {
+
+        #region Base
+
+        public DirectoryWorkerSetFireDateViewModel()
+        {
+            SelectedDirectoryWorkerFireDate = DateTime.Now;
+
+            FireDirectoryWorkerCommand = new RelayCommand(FireDirectoryWorker);
+        }
+
+        private void FireDirectoryWorker(object parameter)
+        {
+            var window = (Window)parameter;
+
+            if (window != null)
+            {
+                window.Close();
+            }
+        }
+
+        #endregion
+
+        #region DirectoryWorkerFireDate
+
+        private DateTime? _selectedDirectoryWorkerFireDate;
+        public DateTime? SelectedDirectoryWorkerFireDate
+        {
+            get 
+            {
+                return _selectedDirectoryWorkerFireDate;
+            }
+            set
+            {
+                _selectedDirectoryWorkerFireDate = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region Commands
+
+        public RelayCommand FireDirectoryWorkerCommand { get; set; }
+
+        public override void ViewClose(object parameter)
+        {
+            //SelectedDirectoryWorkerFireDate = null;
+
+            base.ViewClose(parameter);
+        }
+        #endregion
+
+
+    }
+}
