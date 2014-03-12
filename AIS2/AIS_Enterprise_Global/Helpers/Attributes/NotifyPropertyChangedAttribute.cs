c84ffe36eb34c9e64d76be17a8b011be744268e6
Alexey Breshchenko <breshch@gmail.com>
2014-03-12 14:17:07 +0400

    Магия

---
+++ AIS2/AIS_Enterprise_Global/Helpers/Attributes/NotifyPropertyChangedAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIS_Enterprise_Global.Helpers.Attributes
+{
+    class NotifyPropertyChangedAttribute : Attribute
+    {
+    }
+}
