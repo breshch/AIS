8e6e5d9db0aea2dfeedfe480d5480466e889a17f
Alexey Breshchenko <breshch@gmail.com>
2014-03-12 14:16:17 +0400

    Добавили воркер для байдинга

---
+++ AIS2/AIS_Enterprise_Global/Helpers/Temps/MonthTimeSheetWorker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIS_Enterprise_Global.Helpers.Temps
+{
+    public class MonthTimeSheetWorker
+    {
+        public string FullName { get; set; }
+        public string PostName { get; set; }
+        public double SalaryInHour { get; set; }
+        public List<string> Hours { get; set; }
+
+        public MonthTimeSheetWorker()
+        {
+            Hours = new List<string>();
+        }
+    }
+}
