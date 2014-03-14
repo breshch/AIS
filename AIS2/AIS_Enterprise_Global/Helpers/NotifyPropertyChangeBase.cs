2193d8544452649f547e31a71f956ecfe3aa66cd
Ivan <toporischev@gmail.com>
2014-03-14 13:28:33 +0600

    Класс, реализующий INPC

---
+++ AIS2/AIS_Enterprise_Global/Helpers/NotifyPropertyChangeBase.cs
@@ -0,0 +1,27 @@
+﻿using AIS_Enterprise_Global.Helpers.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIS_Enterprise_Global.Helpers
+{
+    [Magic]
+    public class NotifyPropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
