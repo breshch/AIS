988195c2cbfa4c21b9ac37fc565ac00295479307
Alexey Breshchenko <breshch@gmail.com>
2014-03-20 12:36:46 +0400

    Добавлены свойства

--- AIS2/AIS_Enterprise_Global/Models/Infos/InfoMonth.cs
+++ AIS2/AIS_Enterprise_Global/Models/Infos/InfoMonth.cs
@@ -14,12 +14,14 @@ namespace AIS_Enterprise_Global.Models.Infos
         public int CountVacationDays { get; set; }
         public int CountSickDays { get; set; }
         public int CountMissDays { get; set; }
-        public double PrePayment { get; set; }
-        public double PrePaymentBR { get; set; }
-        public double VacationBR { get; set; }
-        public double SalaryBR { get; set; }
+        public double PrepaymentCash { get; set; }
+        public double PrepaymentBankTransaction { get; set; }
+        public double VocationPayment { get; set; }
+        public double SalaryAV { get; set; }
+        public double SalaryFenox { get; set; }
+        public double Panalty { get; set; }
         public double Inventory { get; set; }
-        public double BirthdayPay { get; set; }
+        public double BirthDays { get; set; }
         public double Bonus { get; set; }
     }
 }
