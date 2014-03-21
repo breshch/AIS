d6e79e51d093dbea2d24f9526274dd6ad66fb0ac
Alexey Breshchenko <breshch@gmail.com>
2014-03-21 13:22:15 +0400

    добавили проверки

--- AIS2/AIS_Enterprise_Global/Helpers/Temps/HourWorker.cs
+++ AIS2/AIS_Enterprise_Global/Helpers/Temps/HourWorker.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace AIS_Enterprise_Global.Helpers.Temps
 {
@@ -28,6 +29,29 @@ namespace AIS_Enterprise_Global.Helpers.Temps
             }
             set
             {
+                value = value.ToUpper().Replace(".", ",");
+
+                if (_value == value)
+                {
+                    return;
+                }
+
+                if (!Enum.IsDefined(typeof(DescriptionDay), value))
+                {
+                    double result;
+                    if (!double.TryParse(value, out result))
+                    {
+                        MessageBox.Show("Введите общепринятые сокращения.");
+                        return;
+                    }
+
+                    if (result <= 0 || result > 16)
+                    {
+                        MessageBox.Show("Введите только число, большее 0 и меньшее, либо равное 16.");
+                        return;
+                    }
+                }
+
                 string prevValue = _value;
 
                 _value = value;
