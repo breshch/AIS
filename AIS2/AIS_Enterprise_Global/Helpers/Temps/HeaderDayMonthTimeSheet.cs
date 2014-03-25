4e54ccdf73d0d4e4310318fb2a9f40b74fdc00d8
Alexey Breshchenko <breshch@gmail.com>
2014-03-25 11:40:46 +0400

    добавили временный класс Header day

---
+++ AIS2/AIS_Enterprise_Global/Helpers/Temps/HeaderDayMonthTimeSheet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIS_Enterprise_Global.Helpers.Temps
+{
+    public class HeaderDayMonthTimeSheet : NotifyPropertyChangedBase
+    {
+        public string Header { get; set; }
+        public bool IsVisible { get; set; }
+    }
+}
