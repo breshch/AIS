881427073b7f46c28dc7aba1c386088d7b72f5b1
Alexey Breshchenko <breshch@gmail.com>
2014-03-25 11:38:56 +0400

    добавили конвертер value to visibility

---
+++ AIS2/AIS_Enterprise_Global/Helpers/ValueConverters/ValueToVisibilityValueConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace AIS_Enterprise_Global.Helpers.ValueConverters
+{
+    public class ValueToVisibilityValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value != null)
+            {
+                var textBlock = (TextBlock)value;
+                Debug.WriteLine(textBlock.Text);
+                return !string.IsNullOrWhiteSpace(textBlock.Text) ? Visibility.Visible : Visibility.Collapsed;
+            }
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
