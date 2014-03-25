16d365827b21caf449f12df98f20f30d438d7300
Alexey Breshchenko <breshch@gmail.com>
2014-03-25 11:39:34 +0400

    добавили конвертер value to bool

---
+++ AIS2/AIS_Enterprise_Global/Helpers/ValueConverters/ValueToBooleanValueConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AIS_Enterprise_Global.Helpers.ValueConverters
+{
+    public class ValueToBooleanValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value != null ? bool.Parse("True") : bool.Parse("False");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
