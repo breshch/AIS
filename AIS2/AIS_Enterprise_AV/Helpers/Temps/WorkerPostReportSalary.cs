280c703ae856fc2d7cbae5eb8f28a626b15a2751
Alexey Breshchenko <breshch@gmail.com>
2014-04-22 12:09:19 +0400

    отчёт зп

---
+++ AIS2/AIS_Enterprise_AV/Helpers/Temps/WorkerPostReportSalary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIS_Enterprise_AV.Helpers.Temps
+{
+    public class WorkerPostReportSalary
+    {
+        public int PostId { get; set; }
+        public string PostName { get; set; }
+        public double AdminWorkerSalary { get; set; }
+        public int ChangePostDay { get; set; }
+        public int CountWorkDays { get; set; }
+        public double CountWorkHours { get; set; }
+        public double CountWorkOverTimeHours { get; set; }
+    }
+}
