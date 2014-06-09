764494e8b1b49441a0a10ec1ab48421df9f6e425
iVan <toporischev@gmail.com>
2014-06-10 00:23:26 +0600

    Правила видимости меню

--- AIS2/AIS_Enterprise_AV/ViewModels/Helpers/InitializingDBViewModel.cs
+++ AIS2/AIS_Enterprise_AV/ViewModels/Helpers/InitializingDBViewModel.cs
@@ -5,6 +5,7 @@ using AIS_Enterprise_Global.Models;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
